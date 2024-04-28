5cf16e22ba6903776175903bf919c0da2e568514
pramod pawar <kingpramodpawar511@gmail.com>
2024-04-28 10:48:39 +0530

    Add a related products section

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -1,31 +1,81 @@
+"use client";
 import ProductDetailsCarousel from "@/components/ProductDetailCrousel";
 import RelatedProducts from "@/components/RelatedProducts";
 import Wrapper from "@/components/Wrapper";
-import React from "react";
+import { fetchApiData } from "@/utils/api";
+import { useEffect, useState } from "react";
 import { IoMdHeartEmpty } from "react-icons/io";
 
-const ProductId = () => {
+// export const expectProducts = async (slug) => {
+//   const products = await fetchApiData(
+//     `/api/products?populate=*&[filters][slug][$ne]=${slug}`
+//   );
+//   return products;
+// };
+// export const particularProduct = async (slug) => {
+//   const product = await fetchApiData(
+//     `/api/products?populate=*&[filters][slug][$eq]=${slug}`
+//   );
+//   return product;
+// };
+
+const ProductId = ({ params }) => {
+  // const product = await particularProduct(params.slug);
+  // const products = await expectProducts(params.slug);
+  const [seleteSize, setSelectSize] = useState();
+  const [showError, setShowError] = useState(true);
+  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  const fetchProducts = async () => {
+    const res = await fetchApiData(
+      `/api/products?populate=*&[filters][slug][$eq]=${params.slug}`
+    );
+    setProduct(res);
+    const ress = await fetchApiData(
+      `/api/products?populate=*&[filters][slug][$ne]=${params.slug}`
+    );
+    setProducts(ress);
+  };
+  if (product.length && products.length === 0) return;
+
+  const p = product?.data[0]?.attributes;
+  const discountAmount = p.orignal_price - p?.price;
+  const discountPercentage = (
+    (discountAmount / p?.orignal_price) *
+    100
+  ).toFixed(0);
   return (
     <div className="w-full md:py-20">
       <Wrapper>
         <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
           {/* left colum start  */}
           <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full max-auto lg:mx-0">
-            <ProductDetailsCarousel />
+            <ProductDetailsCarousel images={p.image.data} />
           </div>
           {/* left colum end */}
           {/* right colum start */}
           <div className="flex-[1] py-3">
             {/* PRODUCT tITLE  */}
-            <div className="text-[34px] font-semibold mb-2">
-              Jordan Retro 6 G
+            <div className="text-[25px] md:text-[34px] font-semibold mb-2">
+              {p?.name}
             </div>
             {/* PRODUCT SUBTITLE  */}
-            <div className="text-lg font-semibold mb-5"> 
-              Men&apos;s Golf Shoes
-            </div>
+            <div className="text-lg font-semibold mb-5">{p?.subtitle}</div>
             {/* PRODUCT PRICE  */}
-            <div className="text-lg font-semibold">MRP : ₹ 1000</div>
+            <div className="flex">
+              <div className="text-lg font-semibold">MRP : ₹{p?.price}</div>
+              <p className="mr-2 text-md font-semibold line-through ml-3">
+                ₹{p?.orignal_price}
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                {discountPercentage}% off
+              </p>
+            </div>
+
             <div className="text-md font-medium text-black/[0.5]">
               incl. of taxes
             </div>
@@ -42,39 +92,37 @@ const ProductId = () => {
               </div>
               {/* SIZE SELECTION  */}
               <div className="grid grid-cols-3 gap-2">
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-                <div className="border cursor-not-allowed rounded-md text-center py-3 font-medium hover:border-black/[0.1] opacity-50">
-                  UK 6
-                </div>
+                {p?.size?.data?.map((item, i) => (
+                  <div
+                    key={i}
+                    className={`border cursor-not-allowed rounded-md text-center py-3 font-medium ${
+                      item.enabled
+                        ? "hover:border-black cursor-pointer"
+                        : "cursor-not-allowed bg-black/[0.1] opacity-50"
+                    } ${seleteSize===item.size ? "border-black":""} `}
+                    onClick={() => {
+                      setSelectSize(item.size);
+                      setShowError(false);
+                    }}
+                  >
+                    {item.size}
+                  </div>
+                ))}
               </div>
               {/* Error PART  */}
-              <div className="text-red-600 mt-1">
-                Size selection is required
-              </div>
+
+              {showError && (
+                <div className="text-red-600 mt-1">
+                  Size selection is required
+                </div>
+              )}
             </div>
             {/* Main section end  */}
             {/* Cart Button  */}
-            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
+            <button onClick={()=>{
+              if(!showError) setShowError(true);
+
+            }} className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
               Add to Cart
             </button>
             <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
@@ -83,25 +131,13 @@ const ProductId = () => {
             <div>
               <div className="text-lg font-bold mb-5">Product Details</div>
               <div className="text-md mb-5">
-                Shoes are more than just pieces of footwear; they are an
-                essential part of our daily lives and a reflection of our
-                personality and style. From the practicality of protecting our
-                feet to the fashion statement they make, shoes serve a multitude
-                of purposes. First and foremost, shoes provide protection and
-                support for our feet as we navigate the world around us. Whether
-                walking on rough terrain, running errands in the city, or
-                engaging in sports activities, the right pair of shoes can make
-                all the difference in comfort and performance. With various
-                styles designed for specific activities, such as running shoes
-                for jogging or hiking boots for outdoor adventures, shoes are
-                tailored to meet the demands of different lifestyles and
-                pursuits.
+              {p?.description[0]?.children[0]?.text}
               </div>
             </div>
           </div>
           {/* right colum end */}
         </div>
-        {/* <RelatedProducts/> */}
+        <RelatedProducts products={products}/>
       </Wrapper>
     </div>
   );
--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -4,7 +4,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import ProductCard from "./ProductCard";
 
-const RelatedProducts = () => {
+const RelatedProducts = ({products}) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -22,15 +22,10 @@ const RelatedProducts = () => {
   return (
     <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
       <Carousel responsive={responsive} containerClass="-mx-[10px]" itemClass="px-[10px]">
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+      {products?.data?.map((card) => (
+            <ProductCard key={card?.id} card={card} />
+          ))}
       </Carousel>
-      ;
     </div>
   );
 };
